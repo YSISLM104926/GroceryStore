c952449435e7d1d467b86c87f62803320402cea7
likhon15 <likhon15-3916@diu.edu.bd>
2024-04-01 08:21:17 +0600

    added

--- src/Components/Shared/Navbar/Navbar.tsx
+++ src/Components/Shared/Navbar/Navbar.tsx
@@ -1,13 +1,36 @@
-import { Container, Stack, Typography } from "@mui/material"
+import { Box, Button, Container, Stack, Typography } from "@mui/material"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
     <>
-     <Container>
-        <Stack>
-            <h1>NextJS </h1>
+      <Container>
+        <Stack
+          direction="row"
+          marginTop="20px"
+          justifyContent="space-between"
+          alignContent="center"
+        >
+          <Typography
+            fontSize="27px"
+            fontWeight={550}
+          >Grocery<span
+            className="text-emerald-500"
+          >Store</span></Typography>
+
+          <Box sx={{
+            display: "flex",
+            gap: "20px"
+          }}>
+            <Button color="inherit"><Link href={'/'}>Home</Link></Button >
+            <Button color="inherit"><Link href={'/'}>Categories</Link></Button >
+            <Button color="inherit"><Link href={'/'}>Products</Link></Button >
+            <Button color="inherit"><Link href={'/'}>Flash Sale</Link></Button >
+            <Button color="inherit"><Link href={'/'}>About Us</Link></Button >
+            <Button color="inherit"><Link href={'/'}>Contact Us</Link></Button >
+          </Box>
         </Stack>
-     </Container>
+      </Container>
     </>
   )
 }
